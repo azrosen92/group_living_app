9fa331436e4314a0861552a0da165658c7e55ca0
Aaron Rosen <azrosen92@gmail.com>
2013-01-08 11:10:11 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+GroupLivingApp::Application.config.secret_token = '1ff24edd4c46aec31a284e9ae872f8ad8e916c1859eda46e63b1d1ece0093936549c93960bee9d2c060ef5180ba0d9d9a8568b7db38cb2d452bdcfe4edfe66da'
